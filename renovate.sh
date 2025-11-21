c6980388d49cd04042fe241c0b571e82c17e92f0
Dominik Holler <dholler@redhat.com>
2025-11-21 10:15:59 +0100

    kubevirt: ENOVATE_BASE_BRANCH_PATTERNS to select bazel version per branch

--- renovate.sh
+++ renovate.sh
@@ -18,7 +18,8 @@ podman run --rm -it --pull=always \
     ghcr.io/renovatebot/renovate
 
 
-buildah build --pull=always  --format docker -t kubevirt-renovate
+
+buildah build --pull=always --build-arg BAZEL_VERSION=6.5.0 --format docker -t kubevirt-renovate-bazel-650
 
 REPO=kubevirt/kubevirt
 #REPO=tmp-kv-mirror/kubevirttest
@@ -32,7 +33,22 @@ podman run --rm -it \
     -e RENOVATE_CONFIG="$(< kubevirt-renovate.json)" \
     -e RENOVATE_ONBOARDING=false \
     -e RENOVATE_REQUIRE_CONFIG=optional \
-    localhost/kubevirt-renovate
+    -e RENOVATE_BASE_BRANCH_PATTERNS='["main", "release-1.7"]' \
+    localhost/kubevirt-renovate-bazel-650
+
+
+buildah build --pull=always --build-arg BAZEL_VERSION=5.4.1 --format docker -t kubevirt-renovate-bazel-541
+podman run --rm -it \
+    -e RENOVATE_FORK_TOKEN=$GITHUB_PAT \
+    -e RENOVATE_TOKEN=$GITHUB_AUTHOR_TOKEN \
+    -e RENOVATE_REPOSITORIES=$REPO \
+    -e LOG_LEVEL=$LOG_LEVEL \
+    -e RENOVATE_ALLOWED_POST_UPGRADE_COMMANDS='["make deps-update"]' \
+    -e RENOVATE_CONFIG="$(< kubevirt-renovate.json)" \
+    -e RENOVATE_ONBOARDING=false \
+    -e RENOVATE_REQUIRE_CONFIG=optional \
+    -e RENOVATE_BASE_BRANCH_PATTERNS='["release-1.6"]' \
+    localhost/kubevirt-renovate-bazel-541
 
 podman run --rm -it \
     -e RENOVATE_FORK_TOKEN=$GITHUB_PAT \
@@ -43,7 +59,7 @@ podman run --rm -it \
     -e RENOVATE_CONFIG="$(< kubevirt-renovate-no-workspace.json)" \
     -e RENOVATE_ONBOARDING=false \
     -e RENOVATE_REQUIRE_CONFIG=optional \
-    localhost/kubevirt-renovate
+    localhost/kubevirt-renovate-bazel-541
 
 
 
